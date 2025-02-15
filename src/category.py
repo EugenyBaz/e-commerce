8b16184212b01d12a75aa52bf8d315117d7f13c5
Bazavod <eugeny.bazavod@list.ru>
2025-02-15 22:07:08 +0300

    create init for product and category

--- src/category.py
+++ src/category.py
@@ -1,28 +1,18 @@
-from src.product import Product
 
 
 class Category:
     name: str
     description: str
     products: list
-    category_count = 0
-    product_count = 0
+    quantity_cat = 0
+    quantity_products = 0
 
-    def __init__(self, name, description, product_list=None):
+    def __init__(self, name, description, products):
         self.name = name
         self.description = description
-        self.products = product_list if product_list else []
-        Category.category_count += 1
-        if product_list:
-            Category.product_count += len(product_list)
+        self.products = products
+        quantity_cat += 1
+        quantity_products += len(products) if products else 0
 
-    def __repr__(self):
-        return f"Category(name='{self.name}', description='{self.description}')"
+if __name__ == "__main__"
 
-
-if __name__ == "__main__":
-    product_list1 = Product("телефон", "черный, 100Гб", 100000.0, 1)
-    category1 = Category("Телевизоры", "SMART телевизоры", [product_list1])
-    print(category1.name)
-    print(category1.description)
-    print(category1.products)
