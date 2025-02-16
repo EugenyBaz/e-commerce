fd210cda614bf2075ae23b9832e8273ad11d31fd
Bazavod <eugeny.bazavod@list.ru>
2025-02-16 12:58:24 +0300

    delete print in category and product

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from src.product import Product
 from src.category import Category
 from src.data_upload import read_data_json
-import json
+
 
 if __name__ == "__main__":
     product1 = Product("Samsung Galaxy S23 Ultra", "256GB, Серый цвет, 200MP камера", 180000.0, 5)
