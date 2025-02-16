f6e59bf289faf1c9daa1b3ab53312642b845640f
Bazavod <eugeny.bazavod@list.ru>
2025-02-16 10:15:39 +0300

    create tests for category and product

--- tests/conftest.py
+++ tests/conftest.py
@@ -7,6 +7,10 @@ from src.product import Product
 @pytest.fixture
 def product_list():
     return Product(name="Iphone 15", description="512GB, Gray space", price=210000.0, quantity=8)
+    return Product(name = "Iphone 15",
+        description = "512GB, Gray space",
+        price = 210000.0,
+        quantity = 8)
 
 
 @pytest.fixture
@@ -20,3 +24,7 @@ def category_empty_prod_list():
     return Category(
         name="Смартфоны", description="Самые лучшие", product_list=[]
     )
+    return Category (
+    name = "Смартфоны",
+    description = "Самые лучшие",
+    product_list = ["Samsung", "Улучшенная версия", 1000, 1])
