0e24d6a143eabe18ae9d869a050340484476bf75
Bazavod <eugeny.bazavod@list.ru>
2025-02-16 10:15:39 +0300

    create tests for category and product

--- src/product.py
+++ src/product.py
@@ -4,7 +4,7 @@ class Product:
     name: str
     description: str
     price: float
-    quantity:
+    quantity: int
 
     def __init__(self, name, description, price, quantity):
         self.name = name
@@ -12,5 +12,16 @@ class Product:
         self.price = price
         self.quantity = quantity
 
+    def __repr__(self):
+        return f'name="{self.name}", description="{self.description}", price={self.price}, quantity={self.quantity})'
 
-if __name__ == "__main__"
+
+if __name__ == "__main__":
+    product1 = Product("телефон", "черный, 100Гб", 100000.0, 1)
+    product2 = Product("холодильник", "белый", 60000.0, 1)
+    product3 = Product("наушники", "красные, BT", 45000.0, 1)
+    product4 = Product("кофеварка", "черный", 78000.0, 1)
+    print(product1.name)
+    print(product2.description)
+    print(product3.price)
+    print(product4.quantity)
